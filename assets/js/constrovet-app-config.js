0d8fd7fb5054309dd099ac635fd0de15a0a04f1e
Taran Bhagat <bhagat.taran@gmail.com>
2026-07-02 17:07:15 +0530

    Configure Apps Script endpoint

--- assets/js/constrovet-app-config.js
+++ assets/js/constrovet-app-config.js
@@ -1,7 +1,7 @@
 (function () {
   window.CONSTROVET_APP_CONFIG = {
     // Paste the deployed Google Workspace Apps Script web app /exec URL here.
-    appsScriptEndpoint: "",
+    appsScriptEndpoint: "https://script.google.com/macros/s/AKfycbwKAbhU2WNR7BSNQS9XMMqhlvYMBb-QwKckfkiAiNIdf4pPD-dBBACO42lE5omKH4E9kQ/exec",
     // Optional nuisance filter. Must match PUBLIC_DEPLOYMENT_KEY in Apps Script when that property is set.
     deploymentKey: "",
     siteVersion: "workspace-apps-script-v1"
